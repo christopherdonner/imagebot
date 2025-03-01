90fb7e3a98e4233a8337cc1c6a48e17668f3a86c
Christopher Donner <cdonner@opentext.com>
2025-03-01 13:48:03 -0500

    on request to root, fs reads /public/img directory and returns an array of the directory listing to handlebars, where they are displayed as images

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ var express = require("express");
 app = express();
 
 const http = require('http'),
-      fs = require('fs'),
-      PORT = 81;
-
-
+  fs = require('fs'),
+  PORT = 81;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -20,23 +18,16 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-  
-// // Use Handlebars to render the main index.html page with the coyotes in it.
-
 
 app.get('/', (req, res) => {
-  // res.send("'Now using http..'");
-  fs.readdir('./src/public/img/', (err, files)=>{
-    console.log(files);
-    for(file of files){}
-    res.render("index", {images: files});
+  // get the directory listing of the images folder
+  fs.readdir('./public/img/', (err, files) => {
+    res.render("index", { images: files });
   })
-
-  
 });
 let options = {},
-    server = http.createServer(options, app);
+  server = http.createServer(options, app);
 
 server.listen(PORT, () => {
- console.log("server starting on port : " + PORT)
+  console.log("server starting on port : " + PORT)
 });
