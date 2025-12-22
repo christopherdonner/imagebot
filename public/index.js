ff2fac12f8531948eeb5e26993db2bbb827f302a
Christopher Donner <cdonner@opentext.com>
2025-12-22 12:58:55 -0500

    added viewer with next and close functionality

--- public/index.js
+++ public/index.js
@@ -1,40 +1,44 @@
-console.log('index.js')
 
-function expando(that, id){
-    // event.preventDefault();
-    // event.stopImmediatePropagation();
+function view(that, id) {
 
-    let key = that.classList,
-        newSRC;
-    let firstHalfSRC = that.src.split('.').splice(0,that.src.split('.').length-3);
-    let extension = that.src.split('.').splice((that.src.split('.').length)-1,that.src.split('.').length-3)[0];
-    if(that.src.includes('thumb')){
+    let viewer = document.querySelector('.viewer'),
+        viewerImage = viewer.querySelector('.image');
+    let o = 1;
 
-        let newSRC = that.src.split('thumb.png')[0].slice(0,that.src.split('thumb.png')[0].length-1);
-        that.src = newSRC;
-    }
-    console.log(that);
-    console.log(key);
+    viewer.classList.remove('hidden');
+    viewerImage.innerHTML = `<img src='${that.src}' onclick='shrinkAll()' class='enhanced-image'/>`
 
-    if(that.classList.contains('big')){
-        that.parentElement.removeAttribute('href');
-        that.classList.remove('big');
-        
-    } else {
-        that.classList.add('big');
-        that.parentElement.setAttribute('href', that.src);
-    }
-}   
+    let nextButton = viewer.querySelector('#next'),
+        prevButton = viewer.querySelector('#prev');
 
-function enhance(that){
+    nextButton.addEventListener('click', function () {
+        let viewerID = id;
+
+        let nextImage = document.querySelector(`img[data-id='${parseInt(viewerID) + o}']`),
+            nextImageSRC = nextImage.getAttribute('full');
+        viewerImage.innerHTML = `<img src='${nextImageSRC}'/>`
+        o++;
+        if (parseInt(viewerID) + o > document.querySelectorAll('img').length) {
+            viewerID = 1;
+        }
+    });
+
+}
+
+function enhance(that) {
     console.log('ehnahce')
     let key = that.classList,
         newSRC;
-    let firstHalfSRC = that.src.split('.').splice(0,that.src.split('.').length-3);
-    let extension = that.src.split('.').splice((that.src.split('.').length)-1,that.src.split('.').length-3)[0];
-    if(that.src.includes('thumb')){
-        let newSRC = that.src.split('thumb.png')[0].slice(0,that.src.split('thumb.png')[0].length-1);
+    let firstHalfSRC = that.src.split('.').splice(0, that.src.split('.').length - 3);
+    let extension = that.src.split('.').splice((that.src.split('.').length) - 1, that.src.split('.').length - 3)[0];
+    if (that.src.includes('thumb')) {
+        let newSRC = that.src.split('thumb.png')[0].slice(0, that.src.split('thumb.png')[0].length - 1);
         that.src = newSRC;
     }
+}
 
+function closeViewer(that) {
+    let viewer = document.querySelector('.viewer');
+    viewer.classList.add('hidden');
+    viewer.querySelector('.image').innerHTML = '';
 }
